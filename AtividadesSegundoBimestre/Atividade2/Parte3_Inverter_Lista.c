34f190bb555b029b86db967c12849d46ef0e6ba1
MateusPresner <mateush2003@yahoo.com>
2026-05-26 16:06:24 -0300

    Update Parte3_Inverter_Lista.c

--- AtividadesSegundoBimestre/Atividade2/Parte3_Inverter_Lista.c
+++ AtividadesSegundoBimestre/Atividade2/Parte3_Inverter_Lista.c
@@ -1,3 +1,51 @@
+typedef struct NoSimples {
+    int valor;
+    struct NoSimples *prox;
+} NoSimples;
+
+/* Criar nó */
+NoSimples* criarNo(int valor) {
+    NoSimples *novo = (NoSimples*) malloc(sizeof(NoSimples));
+
+    if (novo == NULL) {
+        printf("Erro de alocacao!\n");
+        exit(1);
+    }
+
+    novo->valor = valor;
+    novo->prox = NULL;
+
+    return novo;
+}
+
+/* Inserir no final */
+void inserirFinal(NoSimples **head, int valor) {
+    NoSimples *novo = criarNo(valor);
+
+    if (*head == NULL) {
+        *head = novo;
+        return;
+    }
+
+    NoSimples *temp = *head;
+
+    while (temp->prox != NULL) {
+        temp = temp->prox;
+    }
+
+    temp->prox = novo;
+}
+
+/* Exibir lista */
+void exibirLista(NoSimples *head) {
+    while (head != NULL) {
+        printf("%d -> ", head->valor);
+        head = head->prox;
+    }
+
+    printf("NULL\n");
+}
+
 void inverterLista(NoSimples **head) {
     NoSimples *anterior = NULL;
     NoSimples *atual = *head;
