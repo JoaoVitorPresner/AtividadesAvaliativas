5d92b6974a888a19cfa026243980efe9f1a42e46
MateusPresner <mateush2003@yahoo.com>
2026-05-26 15:36:13 -0300

    Update Parte3_Inverter_Lista.c

--- AtividadesSegundoBimestre/Atividade2/Parte3_Inverter_Lista.c
+++ AtividadesSegundoBimestre/Atividade2/Parte3_Inverter_Lista.c
@@ -1,1 +1,15 @@
+void inverterLista(NoSimples **head) {
+    NoSimples *anterior = NULL;
+    NoSimples *atual = *head;
+    NoSimples *proximo = NULL;
 
+    while (atual != NULL) {
+        proximo = atual->prox;
+        atual->prox = anterior;
+
+        anterior = atual;
+        atual = proximo;
+    }
+
+    *head = anterior;
+}
