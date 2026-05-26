fcd1b828accd67fce4237bc06069ae8478d4a817
MateusPresner <mateush2003@yahoo.com>
2026-05-26 15:36:37 -0300

    Update Parte4_Dividir_Lista_em_Duas.c

--- AtividadesSegundoBimestre/Atividade2/Parte4_Dividir_Lista_em_Duas.c
+++ AtividadesSegundoBimestre/Atividade2/Parte4_Dividir_Lista_em_Duas.c
@@ -1,1 +1,30 @@
+void dividirLista(NoSimples *head, NoSimples **lista1, NoSimples **lista2) {
+    if (head == NULL) {
+        *lista1 = NULL;
+        *lista2 = NULL;
+        return;
+    }
 
+    NoSimples *lento = head;
+    NoSimples *rapido = head;
+
+    while (rapido->prox != NULL && rapido->prox->prox != NULL) {
+        lento = lento->prox;
+        rapido = rapido->prox->prox;
+    }
+
+    *lista1 = head;
+    *lista2 = lento->prox;
+
+    lento->prox = NULL;
+}
+
+void liberarLista(NoSimples *head) {
+    NoSimples *temp;
+
+    while (head != NULL) {
+        temp = head;
+        head = head->prox;
+        free(temp);
+    }
+}
