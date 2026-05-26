94b6fb852c16c10c385b39b6e5464ea7fb7a0e6b
MateusPresner <mateush2003@yahoo.com>
2026-05-26 15:35:54 -0300

    Update Parte2_Busca_por_Valor.c

--- AtividadesSegundoBimestre/Atividade2/Parte2_Busca_por_Valor.c
+++ AtividadesSegundoBimestre/Atividade2/Parte2_Busca_por_Valor.c
@@ -1,1 +1,14 @@
+int buscarValor(NoSimples *head, int valor) {
+    int posicao = 0;
 
+    while (head != NULL) {
+        if (head->valor == valor) {
+            return posicao;
+        }
+
+        head = head->prox;
+        posicao++;
+    }
+
+    return -1;
+}
