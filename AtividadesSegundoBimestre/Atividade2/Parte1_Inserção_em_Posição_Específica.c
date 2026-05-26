c80d65bda901b45a01f8b22addc31e93adae748a
MateusPresner <mateush2003@yahoo.com>
2026-05-26 15:35:36 -0300

    Update Parte1_Inserção_em_Posição_Específica.c

--- AtividadesSegundoBimestre/Atividade2/Parte1_Inserção_em_Posição_Específica.c
+++ AtividadesSegundoBimestre/Atividade2/Parte1_Inserção_em_Posição_Específica.c
@@ -1,1 +1,32 @@
+void inserirPosicao(NoSimples **head, int valor, int posicao) {
+    if (posicao < 0) {
+        printf("Posicao invalida!\n");
+        return;
+    }
 
+    NoSimples *novo = criarNo(valor);
+
+    /* Inserir no início */
+    if (posicao == 0) {
+        novo->prox = *head;
+        *head = novo;
+        return;
+    }
+
+    NoSimples *temp = *head;
+    int i = 0;
+
+    while (temp != NULL && i < posicao - 1) {
+        temp = temp->prox;
+        i++;
+    }
+
+    if (temp == NULL) {
+        printf("Posicao invalida!\n");
+        free(novo);
+        return;
+    }
+
+    novo->prox = temp->prox;
+    temp->prox = novo;
+}
